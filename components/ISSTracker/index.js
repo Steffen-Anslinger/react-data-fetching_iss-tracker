c02d0ea93569b9bf1cf5d14af0bd2490cad39f1f
Steffen Anslinger <steffen.anslinger@hotmail.com>
2023-12-15 11:25:39 +0100

    added old fetcher function to see the difference

--- components/ISSTracker/index.js
+++ components/ISSTracker/index.js
@@ -3,14 +3,14 @@ import Controls from "../Controls/index";
 import Map from "../Map/index";
 import useSWR from "swr";
 
+// const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
 const fetcher = async (url) => {
   const res = await fetch(url);
 
-  // If the status code is not in the range 200-299,
-  // we still try to parse and throw it.
   if (!res.ok) {
     const error = new Error("An error occurred while fetching the data.");
-    // Attach extra info to the error object.
+
     error.info = await res.json();
     error.status = res.status;
     throw error;
